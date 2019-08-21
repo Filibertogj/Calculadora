2f00c0c6f078440cfb725eacc8be0c6e61d20d17
Filiberto <filiberto-gonzalezj@unilibre.edu.co>
2019-08-20 22:26:36 -0500

    Comit calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { link } from 'fs';
+import { Local } from 'protractor/built/driverProviders';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+  title = 'CALCULADORA';
+  numero1:number;
+  numero2:number;
+  resultado:number;
+
+  Suma(){
+    this.resultado = this.numero1+this.numero2;
+  }
+  Resta(){
+    this.resultado = this.numero1-this.numero2;
+  }
+  Multiplicacion(){
+    this.resultado = this.numero1*this.numero2;
+  }
+
+  Division(){
+
+    if (this.numero2==0)
+  {
+    alert ("No dividiras por cero")
+  }
+  else
+    this.resultado = this.numero1/this.numero2;
+  }
+
+  Modulo(){
+    this.resultado = (this.numero1 % this.numero2);
+  }
+
+  Reset(){
+    this.resultado = null;
+    this.numero1=null;
+    this.numero2=null;
+  }
+
+
+
+
 }
+
